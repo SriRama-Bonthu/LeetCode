7a561a2b6a39dfb89194c7d6b4caa67266895754
Sri Rama Manikanta Sai Bonthu <23a91a0578@aec.edu.in>
2026-03-08 20:08:59 +0530

    Time: 4 ms (47.41%), Space: 52.3 MB (32.08%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,21 +3,19 @@ class Solution {
         int i=0;
         int j=0;
         int max=0;
-        while(j<nums.length){
-            if(nums[j]==0 && k>0){
-                k--;
-            }
-            else if(nums[j]==0 && k==0){
-                max=Math.max(j-i,max);
-                if(nums[i]==0){
-                    k++;
+        while(i<nums.length){
+            while(j<nums.length && (nums[j]==1 || k>0)){
+                if(nums[j]==0 && k>0){
+                    k--;
                 }
-                i++;
-                continue;
+                j++;
             }
-            j++;
+            max=Math.max(j-i,max);
+            if(nums[i]==0){
+                k++;
+            }
+            i++;
         }
-        max=Math.max(j-i,max);
         return max;
     }
 }
