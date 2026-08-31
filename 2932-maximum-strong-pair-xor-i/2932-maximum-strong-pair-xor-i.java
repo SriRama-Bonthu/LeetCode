6565ffdf9ec21eb6b2075dfa6c10587938252512
Sri Rama Manikanta Sai Bonthu <23a91a0578@aec.edu.in>
2026-08-31 22:08:08 +0530

    update

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.java
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.java
@@ -2,17 +2,17 @@ class Solution {
     public int maximumStrongPairXor(int[] nums) {
         int i=0;
         int j=0;
-        int maxXor=0;
+        int max_Xor=0;
         Arrays.sort(nums);
         for(j=0;j<nums.length;j++){
             i=j;
             while(i<nums.length){
-                 if(Math.abs(nums[j]-nums[i])<=Math.min(nums[j],nums[i]) && (nums[i]^nums[j])>maxXor){
-                maxXor=nums[i]^nums[j];
+                 if(Math.abs(nums[j]-nums[i])<=Math.min(nums[j],nums[i]) && (nums[i]^nums[j])>max_Xor){
+                max_Xor=nums[i]^nums[j];
             }
             i++;
         }
         }
-        return maxXor;
+        return max_Xor;
     }
 }
